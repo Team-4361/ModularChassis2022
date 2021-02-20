b6f4b817bdd0da67f56ef1689dff90b592c92b77
Jayson <jaysonmeribe2004@gmail.com>
2021-02-20 10:22:53 -0500

    Updated some things

--- src/main/java/frc/Library/Autonomous/BallVisionCamera.java
+++ src/main/java/frc/Library/Autonomous/BallVisionCamera.java
@@ -12,24 +12,22 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 //Needs PhotonLib
 
 public class BallVisionCamera {
-    
+
     PhotonCamera photonCamera;
 
     NetworkTableInstance inst;
     NetworkTable rootTable;
 
-    double cameraHeight; 
+    double cameraHeight;
     double cameraPitch;
 
     //Height in meters of the yellow ball from Infinite Recharge
     final double BALLHEIGHT = 0.1778;
 
     /*
-        For this set up please make sure Run NetworkTables Server is flipped on in
-        the Photonvision web UI
-        While creating this I also selected DHCP 
-        Make sure the pipeline index 0 is the pipeline
-        Remember this is only to track balls
+      Our networktablename is photonvision
+      Our cameraName is RoxCam2021-4361
+      NetworkTable server is turned off
     */
     public BallVisionCamera(String networkTableHostName, String cameraName, double mCameraHeight, double mCameraPitch)
     {
@@ -42,11 +40,11 @@ public class BallVisionCamera {
 
         // inst.startClient(networkTableHostName);
         // rootTable = inst.getTable("/photonvision/"+cameraName);
-    
-        photonCamera = new PhotonCamera(cameraName/*rootTable*/);  
+
+        photonCamera = new PhotonCamera(cameraName/*rootTable*/);
 
         photonCamera.setPipelineIndex(0);
-        
+
         System.out.println("Connected");
     }
 
@@ -71,7 +69,7 @@ public class BallVisionCamera {
     //Gets the distance to the best target
     public double getDistanceToTarget(PhotonTrackedTarget target)
     {
-        return PhotonUtils.calculateDistanceToTargetMeters(cameraHeight, BALLHEIGHT, cameraPitch, Math.toRadians(target.getPitch()));
+        return PhotonUtils.calculateDistanceToTargetMeters(cameraHeight, BALLHEIGHT, Math.toRadians(cameraPitch), Math.toRadians(target.getPitch()));
     }
 
     public Boolean hasTargets()
@@ -89,7 +87,7 @@ public class BallVisionCamera {
     }
 
     /*
-     Returns a hashmap of the required info needed to locate a ball and 
+     Returns a hashmap of the required info needed to locate a ball and
       move toward it
     */
     public HashMap<String, Double> getTargetGoal()
